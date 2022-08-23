c43210e8fd7f1641b8275413dd89aff4b778dc3e
Dawid Dymke <dawid584@yahoo.com>
2022-08-23 05:14:08 +0200

    Add result

--- importlogging.py
+++ importlogging.py
@@ -22,7 +22,9 @@ def div(a, b, *args):
     logging.info(f"Dodaję {a} i {b}")
     div_1 = a + b
     return div_1
-    def get_data():
+
+
+def get_data():
     operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
     a = float(input("Podaj pierwszą liczbę: "))
     b = float(input("Podaj drugą liczbę: "))
@@ -41,4 +43,15 @@ def div(a, b, *args):
     # te args w prostszej wersji możesz sobie darować.
 
     return operation, a, b
+
+
+operations = { "1": sum , "2": sub , "3": mul , "4": div}
+
+def main():
+
+    operation, a, b, *args = get_data()
+    result = operations[operation](a, b, *args)
+    print(f"Wynik to {result}")
+  
+main()
 
