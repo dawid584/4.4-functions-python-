2b8da986f0d5c56e3ddddb3fe86dce20f830206f
Dawid Dymke <dawid584@yahoo.com>
2022-08-23 05:10:11 +0200

    Add numbers

--- importlogging.py
+++ importlogging.py
@@ -22,4 +22,23 @@ def div(a, b, *args):
     logging.info(f"Dodaję {a} i {b}")
     div_1 = a + b
     return div_1
+    def get_data():
+    operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+    a = float(input("Podaj pierwszą liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+    
+    if type(a) ==(int) or type(a) ==(float) :
+        logging.info(f"argument {a} jest liczbą")
+    else:
+        logging.info(f"argument {a} nie jest liczbą")
+
+    
+    if type(b) ==(int) or type(b) == (float):
+        logging.info(f"argument {b} jest liczbą")
+    else:
+        logging.info(f"argument {b} nie jest liczbą")
+    # dalej pobierasz argumenty i sprawdzasz czy są liczbami
+    # te args w prostszej wersji możesz sobie darować.
+
+    return operation, a, b
 
