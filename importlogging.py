e0d4b9b739aa7c4eb024e9521db4d51b69c0758e
Dawid Dymke <dawid584@yahoo.com>
2022-08-23 14:25:38 +0200

    fix mistake.

--- importlogging.py
+++ importlogging.py
@@ -10,17 +10,17 @@ def sum(a, b, *args):
 
 def sub(a, b, *args):
     logging.info(f"Dodaję {a} i {b}")
-    sub_1 = a + b
+    sub_1 = a - b
     return sub_1
 
 def mul(a, b, *args):
     logging.info(f"Dodaję {a} i {b}")
-    mul_1 = a + b
+    mul_1 = a * b
     return mul_1
 
 def div(a, b, *args):
     logging.info(f"Dodaję {a} i {b}")
-    div_1 = a + b
+    div_1 = a / b
     return div_1
 
 
